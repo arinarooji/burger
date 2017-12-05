80e8946586e4b5aa377dc1be904866dbc0a8d5dc
Keivon Arooji <karooji0@email.cpcc.edu>
2017-12-05 14:06:03 -0500

    setup

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3000,
+  port: 3306,
   host: "localhost",
   user: "root",
   password: "",
-  database: "burgers"
+  database: "burgers_db"
 });
 
 // Make connection.
