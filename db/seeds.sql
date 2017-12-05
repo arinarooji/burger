5cd3fcf0685a51c43fbb584c9f8a3ff744e44014
Keivon Arooji <karooji0@email.cpcc.edu>
2017-12-05 12:38:27 -0500

    setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert sample burgers.
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple Cheeseburger", false);
