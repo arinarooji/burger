0072ede6f68c8118fc4124439c40a94447109db7
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-07 01:54:18 -0500

    connection reconfig

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  console.log("using localhost");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
 
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-}
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
