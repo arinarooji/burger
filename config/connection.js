49eb1f238f66d82fb4bd99fab63d47fc35996e66
Keivon Arooji <karooji0@email.cpcc.edu>
2018-02-07 00:54:52 -0500

    JAWSDB connection config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  console.log("using localhost");
 
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
